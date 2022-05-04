1d7b87d6965817f3c2454e070d50d2f8cca1ac72
hibibol <fkyohei.juice.1104@gmail.com>
2022-05-05 00:25:40 +0900

    use modal

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import asyncio
 
 from client import bot
-from event import event_command, skill_menu_response
+
+
+from tag import (
+    tag_command,
+    tag_list_command,
+    tag_menu_response,
+    create_tag_command,
+    create_tag_response,
+)
+from event import event_command, event_menu_response
 from skill import skill_command, skill_menu_response
-from tag import tag_command, tag_list_command, tag_menu_response
 from uma_data import update_uma_data
 
+import logging
+
+logging.basicConfig(level=logging.CRITICAL)
 
 loop = asyncio.get_event_loop()
 task2 = loop.create_task(update_uma_data())
--- tag.py
+++ tag.py
@@ -7,10 +7,21 @@ import interactions
 from client import bot
 
 
-def get_tag_data() -> Dict[int, Dict[str, str]]:
+def get_tag_data() -> Dict[str, Dict[str, str]]:
     with open("tag_data.json") as f:
-        tag_data: Dict[int, Dict[str, str]] = json.load(f)
+        tag_data: Dict[str, Dict[str, str]] = json.load(f)
     return tag_data
+
+
+def register_tag(guild_id: int, key: str, value: str):
+    tag_data = get_tag_data()
+
+    if str(guild_id) not in tag_data:
+        tag_data[str(guild_id)] = {}
+    tag_data[str(str(guild_id))][key] = value
+
+    with open("tag_data.json", "w") as f:
+        json.dump(tag_data, f, ensure_ascii=False, indent=2)
 
 
 @bot.command(
@@ -19,6 +30,7 @@ def get_tag_data() -> Dict[int, Dict[str, str]]:
     options=[
         interactions.Option(
             name="key",
+            description="呼び出すためのkey",
             type=interactions.OptionType.STRING,
             required=True,
         ),
@@ -27,14 +39,14 @@ def get_tag_data() -> Dict[int, Dict[str, str]]:
 async def tag_command(ctx: interactions.ComponentContext, key: str):
     tag_data = get_tag_data()
     match_keys = []
-    for tag_key in tag_data.get(ctx.guild_id, {}).keys():
+    for tag_key in tag_data.get(str(ctx.guild_id), {}).keys():
         if re.search(key, tag_key):
             match_keys.append(key)
 
     if len(match_keys) == 0:
         return await ctx.send(f"{key}に一致するtagは見つかりませんでした")
     elif len(match_keys) == 1:
-        return await ctx.send(tag_data[ctx.guild_id][key])
+        return await ctx.send(tag_data[str(ctx.guild_id)][key])
     else:
         i = 0
         while i < len(match_keys):
@@ -55,7 +67,7 @@ async def tag_command(ctx: interactions.ComponentContext, key: str):
 async def tag_menu_response(ctx: interactions.CommandContext, key: str):
     tag_data = get_tag_data()
 
-    if value := tag_data.get(ctx.guild_id, {}).get(key):
+    if value := tag_data.get(str(ctx.guild_id), {}).get(key):
         return await ctx.send(value)
     return await ctx.send(f"{key}に一致するtagは見つかりませんでした")
 
@@ -63,31 +75,28 @@ async def tag_menu_response(ctx: interactions.CommandContext, key: str):
 @bot.command(
     name="create_tag",
     description="tagとして文字列を登録します",
-    options=[
-        interactions.Option(
-            name="key",
-            description="呼び出す際の鍵となる文字列",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-        interactions.Option(
-            name="value",
-            description="呼び出したい文字列",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-    ],
 )
-async def create_tag_command(ctx: interactions.ComponentContext, key: str, value: str):
-    tag_data = get_tag_data()
-
-    if ctx.guild_id not in tag_data:
-        tag_data[ctx.guild_id] = {}
-    tag_data[ctx.guild_id][key] = value
-
-    with open("tag_data.json", "w") as f:
-        json.dump(tag_data, f, ensure_ascii=False, indent=2)
-    return await ctx.send("タグを登録しました")
+async def create_tag_command(ctx: interactions.ComponentContext):
+    await ctx.popup(
+        interactions.Modal(
+            title="tagの作成",
+            custom_id="create_tag_form",
+            components=[
+                interactions.TextInput(
+                    style=interactions.TextStyleType.SHORT,
+                    custom_id="key",
+                    label="呼び出す際の鍵となる文字列",
+                    min_length=1,
+                ),
+                interactions.TextInput(
+                    style=interactions.TextStyleType.PARAGRAPH,
+                    custom_id="value",
+                    label="呼び出したい文字列",
+                    min_length=1,
+                ),
+            ],
+        )
+    )
 
 
 @bot.command(
@@ -105,7 +114,13 @@ async def create_tag_command(ctx: interactions.ComponentContext, key: str, value
 async def tag_list_command(ctx: interactions.ComponentContext, pattern: str = ""):
     tag_data = get_tag_data()
     if keys := "\n".join(
-        key for key in tag_data.get(ctx.guild_id, {}) if re.search(pattern, key)
+        key for key in tag_data.get(str(ctx.guild_id), {}) if re.search(pattern, key)
     ):
         return await ctx.send(keys)  # TODO デカすぎるのでpagenationしたい
     return await ctx.send("タグは見つかりませんでした")
+
+
+@bot.modal("create_tag_form")
+async def create_tag_response(ctx: interactions.CommandContext, key: str, value: str):
+    register_tag(ctx.guild_id, key, value)
+    return await ctx.send("タグを作成しました")
